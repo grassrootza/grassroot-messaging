905db519e800ffe3a073912003b622aaa6233afb
Luke Jordan <luke.jordan@grassroot.org.za>
2017-08-13 21:17:20 +0200

    Config fixing for graphite reporting

--- src/main/java/za/org/grassroot/messaging/config/metrics/ProductionMetricsConfiguration.java
+++ src/main/java/za/org/grassroot/messaging/config/metrics/ProductionMetricsConfiguration.java
@@ -5,9 +5,12 @@ import com.codahale.metrics.graphite.Graphite;
 import com.codahale.metrics.graphite.GraphiteReporter;
 import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
 import com.ryantenney.metrics.spring.config.annotation.MetricsConfigurerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 
 import java.net.InetSocketAddress;
 import java.util.concurrent.TimeUnit;
@@ -15,9 +18,12 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 @EnableMetrics
 @Profile({"production"})
+@PropertySource(value = "file:${grassroot.messaging.properties.path}", ignoreResourceNotFound = true)
 public class ProductionMetricsConfiguration extends MetricsConfigurerAdapter {
 
-    @Value("${grassroot.graphite.host:localhost")
+    private static final Logger logger = LoggerFactory.getLogger(ProductionMetricsConfiguration.class);
+
+    @Value("${grassroot.graphite.host:localhost}")
     private String graphiteHost;
 
     @Value("${grassroot.graphite.port:9000}")
@@ -41,6 +47,7 @@ public class ProductionMetricsConfiguration extends MetricsConfigurerAdapter {
     }
 
     private Graphite graphite() {
+        logger.info("starting up graphite, host = {}, port = {}", graphiteHost, graphitePort);
         return new Graphite(new InetSocketAddress(graphiteHost, graphitePort));
     }
 

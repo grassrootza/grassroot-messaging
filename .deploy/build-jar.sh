4a8bfe6df8d1af27a6b6ef522086ad64189b7ebe
Carlos Santini <carlos@digitalassemb.ly>
2018-01-12 23:45:48 +1000

    localhost docker setup

---
+++ .deploy/build-jar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# BUILD JAR FILES
+# ./gradlew clean build -x test -g /usr/src/grassroot/.gradle/tmp --configure-on-demand --parallel --daemon
+./gradlew build -x test -g /usr/src/grassroot-msg/.gradle/tmp --configure-on-demand --parallel --daemon
